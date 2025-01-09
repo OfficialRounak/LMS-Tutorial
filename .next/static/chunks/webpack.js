dec96be8ba66982a0a3bb1f1176b2129ac66287a
RounakCisco <roubhatt@cisco.com>
2025-01-10 02:19:57 +0530

    completed course table and course search in teachers view

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/chunks/" + chunkId + ".js";
+/******/ 			return undefined;
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "86af376fd08acfc3"; }
+/******/ 		__webpack_require__.h = function() { return "4883ad85f792e683"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,30 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 import React from 'react';
+import { DataTable } from './_components/DataTable';
+import { columns } from './_components/Columns';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-const CoursesPage = () => {
+
+const CoursesPage = async() => {
+
+  const {userId} = auth()
+
+  if(!userId){
+    return redirect("/")
+  }
+
+  const data= await db.course.findMany({
+    where : { 
+      userId,
+    }, orderBy : {
+      createdAt : "desc"
+    }
+  })
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+     <DataTable columns={columns} data={data} />
     </div>
   );
 };
